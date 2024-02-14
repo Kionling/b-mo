8e0115dcb67b7025dcad88ef7a9efd4063d459f5
kionling <djvdjv209@gmail.com>
2024-02-14 10:24:39 -0800

    Text to speech is functional, working on speech file removal

---
+++ text_to_speech.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+import os
+
+def text_to_speech(text):
+    speech = gTTS(text)
+    speech_file = "speech.mp3"
+    speech.save(speech_file)
+    os.system('afplay ' + speech_file)
+
+text_to_speech('Hello, world!')
